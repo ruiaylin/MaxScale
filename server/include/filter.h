a26e500bd7c63e66ad030d1d4934a72c9a57654d
Markus Makela <markus.makela@mariadb.com>
2016-08-16 17:13:59 +0300

    Introduce new module declaration macros
    The module declaration macros give a standardized way to declare a module
    of some type. This will remove some part of the guesswork in creating new
    modules. A new module could now be implemented by looking at the headers
    alone since they provide all the required information (or should provide
    if they don't).
    
    The next step is to further document the interfaces and then take these
    macros and structures into use.

--- server/include/filter.h
+++ server/include/filter.h
@@ -83,7 +83,7 @@ typedef struct filter_object
  * is changed these values must be updated in line with the rules in the
  * file modinfo.h.
  */
-#define FILTER_VERSION  {1, 1, 0}
+#define MXS_FILTER_VERSION  {1, 1, 0}
 /**
  * The definition of a filter from the configuration file.
  * This is basically the link between a plugin to load and the
